82db2a4a452247f342db42f8889f572f854f83a9
Shwetha A <shwethaadiraj@gmail.com>
2025-12-27 15:43:22 +0530

    refactor component map import for better readability

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -5,9 +5,7 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 import styles from "./postSlug.module.css";
 import { loadBlogPost } from "@/helpers/file-helpers";
 import { BLOG_TITLE } from "@/constants";
-import CodeSnippet from "@/components/CodeSnippet";
-import DivisionGroupsDemo from "@/components/DivisionGroupsDemo";
-import CircularColorsDemo from "@/components/CircularColorsDemo";
+import COMPONENT_MAP from "@/helpers/mdx-helpers";
 
 export async function generateMetadata({ params }) {
   const { postSlug } = await params;
@@ -30,14 +28,7 @@ async function BlogPost({ params }) {
         />
       )}
       <div className={styles.page}>
-        <MDXRemote
-          source={content}
-          components={{
-            pre: CodeSnippet,
-            DivisionGroupsDemo: DivisionGroupsDemo,
-            CircularColorsDemo: CircularColorsDemo,
-          }}
-        />
+        <MDXRemote source={content} components={COMPONENT_MAP} />
       </div>
     </article>
   );
